188918383e621237130bded1167d7b6e2a9843e1
samuel do prado almeida <samuelprado.a@gmail.com>
2019-12-19 11:55:04 -0300

    Create config.inc.php
    local onde o usuário irá setar as informações

---
+++ app/config.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+define('CONFIG', array(
+
+ /**
+ * DATABASE CONNECTION
+ */
+
+ 'hostname' => 'localhost',
+ 'username' => 'root',
+ 'password' => '',
+ 'database' => 'easy',
+
+ /**
+ * SITENAME
+ * -- host:  yoursite.com.br or yoursite.com.br/sub
+ * -- local: localhost or localhost/sub
+ */
+
+ 'sitename' => 'localhost',
+
+ /**
+ * PROTOCOL
+ * http://
+ * https://
+ */
+
+ 'protocol' => 'http://',
+
+ /**
+ * THEME AND RESOURCES
+ */
+
+ 'themedir' => 'default',
+ 'template' => 'default',
+ 'resource' => 'default'
+
+));
