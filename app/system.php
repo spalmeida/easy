c4abe4296c342bf46b96f87bb26e1b75c49c4f37
samuel do prado almeida <samuelprado.a@gmail.com>
2019-12-19 11:56:30 -0300

    Create system.php
    inicialização e carregamento das classes principais para utilização do sistema

---
+++ app/system.php
@@ -0,0 +1,17 @@
+<?php
+
+//protected
+if (strcmp(basename($_SERVER['SCRIPT_NAME']), basename(__FILE__)) === 0){
+	header('location:'.CONFIG['protocol'].$_SERVER[HTTP_HOST]);
+}
+
+require_once 'vendor/autoload.php';
+
+use EASY\Connect;
+use EASY\Query;
+
+
+$connect  = new Connect();
+$conn = $connect->conn(CONFIG['hostname'],CONFIG['username'], CONFIG['password'],CONFIG['database']);
+
+require_once BASE.'/routes/routes.php';
