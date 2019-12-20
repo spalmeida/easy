1e76e6bdcadff392cbe217749b6c88bd61a31447
samuel do prado almeida <samuelprado.a@gmail.com>
2019-12-20 08:42:00 -0300

    Update pages.php
    retorna a página solicitada na rota

--- app/functions/groups/Pages/pages.php
+++ app/functions/groups/Pages/pages.php
@@ -1,6 +1,8 @@
 <?php
 
-function view($dir){
+function view($dir, $param){
 	$caminho = BASE.DS.'public'.DS.'themes'.DS.CONFIG['themedir'].DS.str_replace('.', DS, $dir).'.php';
+	get_header();
 	file_exists($caminho)? require_once $caminho : '';
+	get_footer();
 }
